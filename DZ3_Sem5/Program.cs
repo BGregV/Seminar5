782ce19867748ffa744433d5a434da6e6c14a7aa
BGregV <greguar@ninodom.ru>
2022-11-01 15:16:44 +0300

    Add Sem5.3

--- DZ3_Sem5/Program.cs
+++ DZ3_Sem5/Program.cs
@@ -1,12 +1,34 @@
-﻿Console.Write("Введите число: ");
-int chislo = int.Parse(Console.ReadLine() ?? "0");
-int fib1=1;
-int fib2=1;
-int fib3=1;
-while(fib3<chislo)
+﻿//Написать программу копирования массива
+void Zapolnenie_Massiva(int[] massiv)
 {
-    Console.Write($"{fib3} ");
-    fib3=fib1+fib2;
-    fib1=fib2;
-    fib2=fib3;
+    Random Znach = new Random();
+    for (int ind = 0; ind < massiv.Length; ind++)
+    {
+        massiv[ind] = Znach.Next(-9, 10);
+    }
 }
+void Pechat_Massiva(int[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+int[] Kopirovanie_massiva(int[] masiv)
+{
+    int[] Kopmas = new int [masiv.Length];
+    for (int ind = 0; ind < masiv.Length; ind++)
+    {
+        Kopmas[ind] = masiv[ind];
+    }
+    return Kopmas;
+}
+Console.Write("Введите длинну массива: ");
+int dlinna = int.Parse(Console.ReadLine()?? "0");
+int[] Array = new int[dlinna];
+Zapolnenie_Massiva(Array);
+Console.Write("Начальный массив: ");
+Pechat_Massiva(Array);
+Console.Write("Скопированный массив: ");
+int[] Array_copy = Kopirovanie_massiva(Array);
+Pechat_Massiva(Array_copy);
